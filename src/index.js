f8fabbd2a6ebb72e0400cbe6976fd904aad375b8
ChornyiYevhenii <gibridius16@gmail.com>
2023-01-16 17:34:23 +0200

    add file

--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const DEBOUNCE_DELAY = 300;
 
 
-const searchCountry = document.querySelector('#search-box');
+const searchCount = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
-searchCountry.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+searchCount.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch(evt) {
   evt.preventDefault();
-  const searchCountryValue = searchCountry.value.trim();
+  const searchCountryValue = searchCount.value.trim();
   fetchCountries(searchCountryValue)
     .then(returnedCountries)
     .catch(error => {
